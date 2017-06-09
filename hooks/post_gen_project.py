6bd1469beb1ed9e928d88e0c0e5269be7bdf8ed8
Carlos Avila <cavila@mandelbrew.com>
2017-06-09 02:12:50 -0700

    Added secrets generation for staging and prod.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,37 +16,37 @@ import sys
 from cookiecutter.main import cookiecutter
 
 # Constants
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-
+PROJECT_DIR = os.path.realpath(os.path.curdir)
+SECRETS_DIR = os.path.join(PROJECT_DIR, 'secrets')
 COMMON_SECRETS = {
-        # Project
-        "PROJECT_NAME": "{{ cookiecutter.project_name }}",
-        "PROJECT_SLUG": "{{ cookiecutter.project_slug }}",
+    # Project
+    "PROJECT_NAME": "{{ cookiecutter.project_name }}",
+    "PROJECT_SLUG": "{{ cookiecutter.project_slug }}",
 
-        # Webpack
-        "WEBPACK_OUTPUT": "./application/static",
+    # Webpack
+    "WEBPACK_OUTPUT": "./application/static",
 
-        # Django
-        "DJANGO_EMAIL_HOST": "smtp.{{ cookiecutter.project_domain }}",
-        "DJANGO_EMAIL_PORT": "587",
-        "DJANGO_EMAIL_HOST_USER": "{{ cookiecutter.project_name }}@{{ cookiecutter.project_domain }}",
-        "DJANGO_DEFAULT_FROM_EMAIL": "contact@{{ cookiecutter.project_domain }}",
+    # Django
+    "DJANGO_EMAIL_HOST": "smtp.{{ cookiecutter.project_domain }}",
+    "DJANGO_EMAIL_PORT": "587",
+    "DJANGO_EMAIL_HOST_USER": "{{ cookiecutter.project_name }}@{{ cookiecutter.project_domain }}",
+    "DJANGO_DEFAULT_FROM_EMAIL": "contact@{{ cookiecutter.project_domain }}",
 
-        # AWS
-        "AWS_ACCESS_KEY_ID": "{{cookiecutter.project_slug|upper()}}_KEY_ID",
-        "AWS_SECRET_ACCESS_KEY": "{{cookiecutter.project_slug|upper()}}_ACCESS_KEY",
-        "AWS_S3_HOST": "s3.amazonaws.com",
-        "AWS_S3_BUCKET": "{{ cookiecutter.project_slug }}-assets",
+    # AWS
+    "AWS_ACCESS_KEY_ID": "{{cookiecutter.project_slug|upper()}}_KEY_ID",
+    "AWS_SECRET_ACCESS_KEY": "{{cookiecutter.project_slug|upper()}}_ACCESS_KEY",
+    "AWS_S3_HOST": "s3.amazonaws.com",
+    "AWS_S3_BUCKET": "{{ cookiecutter.project_slug }}-assets",
 
-        # DB
-        "DB_NAME": "{{ cookiecutter.project_slug }}",
-        "DB_USER": "{{ cookiecutter.project_slug }}",
-        "DB_HOST": "postgres",
-        "DB_PORT": "5432",
+    # DB
+    "DB_NAME": "{{ cookiecutter.project_slug }}",
+    "DB_USER": "{{ cookiecutter.project_slug }}",
+    "DB_HOST": "postgres",
+    "DB_PORT": "5432",
 
-        # Analytics
-        "GOOGLE_ANALYTICS_PROPERTY_ID": "UA-XXXXXX-2",
-    }
+    # Analytics
+    "GOOGLE_ANALYTICS_PROPERTY_ID": "UA-XXXXXX-2",
+}
 
 
 def get_random_string(
@@ -61,11 +61,29 @@ def get_random_string(
     return ''.join(random.SystemRandom().choice(allowed_chars) for i in range(length))
 
 
-def generate_secrets():
+def write_secrets(path, name, secrets):
+    env_f = os.path.join(path, '{0}.env'.format(name))
+    exp_f = os.path.join(path, '{0}.export'.format(name))
 
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if os.path.exists(env_f):
+        print('ERROR: {0} already exists. Refusing to overwrite.'.format(env_f))
+    else:
+        with open(env_f, 'w') as f:
+            f.write("".join(["{0}={1}\n".format(x, secrets[x]) for x in sorted(secrets)]))
+
+    if os.path.exists(exp_f):
+        print('ERROR: {0} already exists. Refusing to overwrite.'.format(exp_f))
+    else:
+        with open(exp_f, 'w') as f:
+            f.write("".join(["export {0}='{1}'\n".format(x, secrets[x]) for x in sorted(secrets)]))
+
+
+def generate_secrets():
     # Development
 
-    dev_target = os.path.join(PROJECT_DIRECTORY, 'development.env')
     dev_secrets = COMMON_SECRETS.copy()
     dev_secrets.update({
         # Webpack
@@ -91,12 +109,49 @@ def generate_secrets():
         "UNISON_IGNORE": "Name {.DS_Store,*.pyc}",
     })
 
-    if os.path.exists(dev_target):
-        print('ERROR: {0} already exists. Refusing to overwrite.'.format(dev_target))
-        sys.exit(1)
-    else:
-        with open(dev_target, 'w') as f:
-            f.write("".join(["{0}={1}\n".format(x, dev_secrets[x]) for x in sorted(dev_secrets)]))
+    write_secrets(SECRETS_DIR, 'development', dev_secrets)
+
+    # Staging
+
+    stg_secrets = COMMON_SECRETS.copy()
+    stg_secrets.update({
+        # Webpack
+        "WEBPACK_CONFIG": "./assets/webpack.staging.config.js",
+
+        # Docker
+        "DOCKER_IMAGE": "{{ cookiecutter.project_name }}-staging",
+        "DOCKER_COMPOSE": "./docker/docker-compose.staging.yml",
+
+        # Django
+        "DJANGO_SETTINGS_MODULE": "config.settings.default",
+        "DJANGO_EMAIL_HOST_PASSWORD": get_random_string(64, string.ascii_letters + string.digits),
+        "DJANGO_SECRET_KEY": get_random_string(),
+        "DJANGO_HONEYPOT_FIELD_NAME": get_random_string(16, string.ascii_letters + string.digits),
+        "DJANGO_MEDIA_URL": "/staging/media/",
+
+        # DB
+        "DB_PASSWORD": get_random_string(64, string.ascii_letters + string.digits),
+    })
+
+    write_secrets(SECRETS_DIR, 'staging', stg_secrets)
+
+    # Production
+
+    prod_secrets = stg_secrets.copy()  # Notice this difference
+    prod_secrets.update({
+        # Webpack
+        "WEBPACK_CONFIG": "./assets/webpack.production.config.js",
+
+        # Docker
+        "DOCKER_IMAGE": "{{ cookiecutter.project_name }}-production",
+        "DOCKER_COMPOSE": "./docker/docker-compose.production.yml",
+
+        # Django
+        "DJANGO_SETTINGS_MODULE": "config.settings.production",
+        "DJANGO_MEDIA_URL": "/production/media/",
+    })
+
+    write_secrets(SECRETS_DIR, 'production', prod_secrets)
 
 
 generate_secrets()
