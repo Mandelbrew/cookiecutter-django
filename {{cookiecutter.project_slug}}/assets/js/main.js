b321e64d0ed8b905a2c3b5cdadd3fbda2c316c4b
Carlos Avila <cavila@mandelbrew.com>
2017-06-23 15:44:02 -0700

    Updated assets template, scripts folder, dockerfile and package.json. Added docs.

--- {{cookiecutter.project_slug}}/assets/js/main.js
+++ {{cookiecutter.project_slug}}/assets/js/main.js
@@ -12,9 +12,16 @@ import "babel-polyfill";
 
 //  Vendors
 // --------------------------
-import "tether";
+import "tether"; // Bootstrap 4 requirement
 import "bootstrap";
+import "holderjs"; // Image placeholders
 
 $(document).ready(() => {
     document.documentElement.classList.remove('no-js');
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
 });
