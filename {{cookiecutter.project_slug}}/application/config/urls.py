777b7ce4d2fc51fd267e5785e9a183bb4e8e8e96
Carlos Avila <cavila@mandelbrew.com>
2020-02-23 02:22:20 -0800

    updates to template and fixing permissions

--- {{cookiecutter.project_slug}}/application/config/urls.py
+++ {{cookiecutter.project_slug}}/application/config/urls.py
@@ -27,10 +27,6 @@ from django.views.defaults import page_not_found, server_error
 admin.site.site_header = 'Site Administration'
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
-    url(r'^400/$', TemplateView.as_view(template_name='400.html')),
-    url(r'^403/$', TemplateView.as_view(template_name='403.html')),
-    url(r'^404/$', page_not_found),
-    url(r'^500/$', server_error),
     url(r'^sitemap\.xml$', sitemap),
     url(r'^dadmin/', include(admin.site.urls)),
     url(r'^wadmin/', include(wagtailadmin_urls)),
@@ -42,4 +38,11 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+    urlpatterns = [
+                      url(r'^400/$', TemplateView.as_view(template_name='400.html')),
+                      url(r'^403/$', TemplateView.as_view(template_name='403.html')),
+                      url(r'^404/$', page_not_found),
+                      url(r'^500/$', server_error),
+                      url(r'^styleguide/$', TemplateView.as_view(template_name='core/styleguide.html')),
+                      url(r'^__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
